51b5f36f186e42406a747056bf921da9bc3b7511
aleri-a <aleksandra.aleri@gmail.com>
2019-12-28 03:41:06 +0100

    Nikola's classes

--- app/src/main/java/com/food/review/ProfileFragment.kt
+++ app/src/main/java/com/food/review/ProfileFragment.kt
@@ -1,19 +1,40 @@
 package com.food.review
 
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDate.of
+import java.time.LocalDate.parse
+import java.time.Month
+import java.util.*
 
 
 class ProfileFragment : Fragment() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     @Nullable
     override fun onCreateView(@NonNull inflater:LayoutInflater, @Nullable container:ViewGroup?, @Nullable savedInstaceState:Bundle?):View?{
+
+        var korisnik=customer("asd","sddd","dsd","111")
+
+
+        val simpleDate = SimpleDateFormat("dd/MM/yyyy")
+
+
+       var rev=review(korisnik,5.0f,Date(1,1,1),"okomenesve")
+        korisnik.addReview(rev)
+        val strDt = simpleDate.format(rev.date)
+        Log.d("111 review+ customer", korisnik.lastName + strDt + rev.user.lastName + korisnik.reviews[0].text) //+ rev.grade.toString() + rev.date.toString() )
         return inflater.inflate(R.layout.fragment_profile,container,false)
     }
 }
