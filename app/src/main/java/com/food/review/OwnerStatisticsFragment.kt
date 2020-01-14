1ed050d626ec0cdc8796a3abf2d932775c43eef6
j-juric <juric.d.jovan@gmail.com>
2020-01-14 20:20:27 +0100

    Ispravljen manifest

--- app/src/main/java/com/food/review/OwnerStatisticsFragment.kt
+++ app/src/main/java/com/food/review/OwnerStatisticsFragment.kt
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.github.mikephil.charting.charts.LineChart
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +30,8 @@ class OwnerStatisticsFragment : Fragment() {
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
+    var lineChart: LineChart? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -39,8 +44,27 @@ class OwnerStatisticsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_owner_statistics, container, false)
+        var v =inflater.inflate(R.layout.fragment_owner_statistics, container, false)
+
+        val btn_top5c = v.findViewById<View>(R.id.btn_top5_count)
+        val btn_top5r = v.findViewById<View>(R.id.btn_top5_rating)
+        val btn_bot5c = v.findViewById<View>(R.id.btn_bottom5_count)
+        val btn_bot5r = v.findViewById<View>(R.id.btn_bottom5_rating)
+        val rad =  v.findViewById<RadioGroup>(R.id.radio_buttons)
+        val radw = v.findViewById<RadioButton>(R.id.radio_week)
+        val radm = v.findViewById<RadioButton>(R.id.radio_month)
+        val rady =  v.findViewById<RadioButton>(R.id.radio_year)
+        rad.check(radm.id)
+        lineChart=v.findViewById<LineChart>(R.id.chart)
+
+        lineChart!!.setTouchEnabled(false)
+
+        rad.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { radioGroup,i->
+        })
+
+
+
+        return v
     }
 
     // TODO: Rename method, update argument and hook method into UI event
